7d3c7d6d70f803976cd1ce03f167485d760f4050
Jack Tattersall <jacktattersall1@gmail.com>
2016-02-28 15:52:19 +0000

    numeralsConverter v1

--- src/main/scala-2.11/RomNums.scala
+++ src/main/scala-2.11/RomNums.scala
@@ -3,4 +3,17 @@
   */
 object RomNums {
 
+  val numerals = List(("M", 1000), ("CM", 900), ("D", 500), ("CD", 400),
+    ("C", 100),  ("XC", 90),  ("L", 50),  ("XL", 40),
+    ("X", 10),   ("IX", 9),   ("V", 5),   ("IV", 4),
+    ("I", 1))
+
+  def numeralsConverter(n: Int): String={
+
+    def go(n:Int, holder: Int, i: Int, input: List[(String, Int)], output: List[String]): List[String] ={
+      if(n==i) output
+      else go(n, holder - input.head._2, i + input.head._2, input.filter(_._2 <= (holder - input.head._2)), input.head._1::output)
+    }
+    go(n, n, 0, numerals.filter(_._2 <= n), List[String]()).reverse.mkString
+  }
 }
